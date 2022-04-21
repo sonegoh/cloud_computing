bc5d763605417c68eb911fa992bd8baca8665b86
Hadar Sonego <sonegoh@gmail.com>
2022-04-21 13:41:48 +0300

    syntax

--- setup.sh
+++ setup.sh
@@ -67,4 +67,4 @@ aws lambda add-permission --function-name $FUNC_NAME \
 echo "This is your api gateway - $API_ENDPOINT"
 echo "Use $API_ENDPOINT/entry?plate=XXX-XXX-XXX&parkingLot=XXX for entrance api"
 echo "The output of this call will be your ticketID, save it for exit."
-echo "Use $API_ENDPOINT/exit??ticketId=XXX for exit api"
+echo "Use $API_ENDPOINT/exit?ticketId=XXX for exit api"
