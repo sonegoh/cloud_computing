c80bd83815450ef5356dedf910bd7103fc578b46
Hadar Sonego <sonegoh@gmail.com>
2022-05-25 17:37:16 +0300

    wip

--- hw2/deploy.sh
+++ hw2/deploy.sh
@@ -1,7 +1,7 @@
-KEY_NAME="hadar-`date +'%F'`"
+KEY_NAME="hadar6-`date +'%F'`"
 KEY_PEM="$KEY_NAME.pem"
 
-STACK_NAME="hadar-stack"
+STACK_NAME="hadar-stack4"
 
 echo "create key pair $KEY_PEM to connect to instances and save locally"
 aws ec2 create-key-pair --key-name $KEY_NAME  --query "KeyMaterial" --output text > $KEY_PEM
@@ -44,7 +44,9 @@ echo $OUTPUTS
 
 echo "getting instances IP"
 PUBLIC_IP_1=$(aws cloudformation --region $REGION describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='Instance1IP'].OutputValue" --output text)
+PRIVATE_IP_1=$(aws cloudformation --region $REGION describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='Instance1PrivateIp'].OutputValue" --output text)
 PUBLIC_IP_2=$(aws cloudformation --region $REGION describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='Instance2IP'].OutputValue" --output text)
+PRIVATE_IP_2=$(aws cloudformation --region $REGION describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='Instance2PrivateIp'].OutputValue" --output text)
 INSTANCE_ID_1=$(aws cloudformation --region $REGION describe-stacks --stack-name $STACK_NAME --query "Stacks[0].Outputs[?OutputKey=='InstanceId1'].OutputValue" --output text)
 SG_FOR_WORKERS=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID_1 --query 'Reservations[*].Instances[*].[SecurityGroups[].GroupId |[*]]' --output text)
 
@@ -52,25 +54,33 @@ echo "First instance ID - "
 echo $INSTANCE_ID_1
 echo "Security group ID used for the stack (app ec2's and workers)"
 echo $SG_FOR_WORKERS
-echo "First instance IP - "
+echo "First instance public IP - "
 echo $PUBLIC_IP_1
-echo "Second instance ID - "
+echo "First instance private IP - "
+echo $PRIVATE_IP_1
+echo "Second instance IP - "
 echo $PUBLIC_IP_2
+echo "Second instance private IP - "
+echo $PRIVATE_IP_2
 
 
-echo "waiting 200 seconds for all the dependencies to install, user data scripts takes time."
-sleep 200
+echo "waiting 300 seconds for all the dependencies to install, user data scripts takes time."
+sleep 500
 
 echo "running the code on first ec2"
-ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_1 'cd cloud_computing/hw2 && python3 main.py $PUBLIC_IP_2' -eaf
+ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_1 "nohup python3 /home/ubuntu/cloud_computing/hw2/main.py $PRIVATE_IP_2 > /dev/null 2>&1 &"
+echo "changing redis conf"
 ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_1 'cd cloud_computing/hw2 && sudo mv redis.conf /etc/redis/redis.conf'
-ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_1 'sudo service redis restart'
-ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_1 "cd cloud_computing/hw2 && python3 auto_scaler.py $KEY_NAME $SG_FOR_WORKERS $PUBLIC_IP_1" -eaf
+echo "restarting redis"
+ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_1 'sudo service redis-server restart'
+#ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_1 "python3 /home/ubuntu/cloud_computing/hw2/auto_scaler.py $KEY_NAME $SG_FOR_WORKERS $PRIVATE_IP_1" -eaf
 echo "running the code on second ec2"
-ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_2 'cd cloud_computing/hw2 && python3 main.py $PUBLIC_IP_1' -eaf
+ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_2 "nohup python3 /home/ubuntu/cloud_computing/hw2/main.py $PRIVATE_IP_1 > /dev/null 2>&1 &"
+echo "changing redis conf"
 ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_2 'cd cloud_computing/hw2 && sudo mv redis.conf /etc/redis/redis.conf'
-ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_2 'sudo service redis restart'
-ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_2 "cd cloud_computing/hw2 && python3 auto_scaler.py $KEY_NAME $SG_FOR_WORKERS $PUBLIC_IP_2" -eaf
+echo "restarting redis"
+ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_2 'sudo service redis-server restart'
+#ssh -i $KEY_PEM -o "StrictHostKeyChecking=no" -o "ConnectionAttempts=10" ubuntu@$PUBLIC_IP_2 "cd cloud_computing/hw2 && python3 auto_scaler.py $KEY_NAME $SG_FOR_WORKERS $PUBLIC_IP_2" -eaf
 
 echo "done"
 
